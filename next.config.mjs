2fd532d83b9207a7c087fcac6e869b1f606089db
shobanasreedharan <shobana.sreedharan@gmail.com>
2026-06-23 19:35:25 -0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,25 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "img-src * data: blob:;",
+          },
+        ],
+      },
+    ]
   },
 }
-
 export default nextConfig
